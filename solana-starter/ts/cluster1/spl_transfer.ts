99733d8de59f7ee7bf9e1d412dc021666b8851f1
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-02 18:46:17 -0700

    day 2

--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "../Turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,25 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("H3yGGGSFLjbFBWPYT2Mg69TJteKnZmFGNBWULrPtFrit");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("8rhBeTmTAimzbmfXUBC2mVhUSwT7LQM1KfwCcKPkZVHk"); //from prereq 4aceMBu9SXiRNvw1CrYjGnXoUraitRRndBawFvSJUSgw
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromATA = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+         const toATA = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to); // owner is to(recipient address)
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const signature = await transfer(connection, keypair, fromATA.address, toATA.address, keypair.publicKey, 101);
+
+        console.log(`trasnsfer: ${signature}`); //5CWtstGdWKFCUaZWhwEYHehjpguKCdQiDWoCuXZRhwgVLRjJZ85HL9zdwLKdk7Mopp8TEhsT4Vvmw7AwoRqf3a44
+ 
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
