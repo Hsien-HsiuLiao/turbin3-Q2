3126372127dc6928692ac982f85a6c57ccda68aa
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-10 00:28:25 -0700

    create instructions

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,4 +1,9 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
+
+mod instructions;
+use instructions::*;
+mod state;
 
 declare_id!("CLe5CJoAo3YYRwgsQsAmPokqKHAw7XF9dJNCBvW25QCL");
 
@@ -6,11 +11,15 @@ declare_id!("CLe5CJoAo3YYRwgsQsAmPokqKHAw7XF9dJNCBvW25QCL");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        deposit();
+        withdraw_and_close();
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
