1013a44d5069d66dde69caad56a2f026594aa5b1
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-11 23:53:17 -0700

    Deposit struct

---
+++ anchor-amm/programs/amm/src/instructions/deposit.rs
@@ -0,0 +1,97 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount}
+};
+use constant_product_curve::ConstantProduct;
+
+use crate::state::Config;
+
+// Anchor programs always use 8 bits for the discriminator
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8; // written to every account on the blockchain by anchor, specifies the type of account, used by anchor for some of its checks
+                                                // so this will be 8 bytes
+
+#[derive(Accounts)]                                                
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    //need ata for user
+    #[account(
+       
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+
+    )]
+    pub user_x: Account<'info, TokenAccount>, 
+    #[account(
+       
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+
+    )]
+    pub user_y: Account<'info, TokenAccount>, 
+
+    pub mint_x: Account<'info, Mint>, 
+    pub mint_y: Account<'info, Mint>,
+    #[account(
+        
+        seeds = [b"lp", config.key().as_ref()], 
+        bump = config.lp_bump
+       
+    )]
+    pub mint_lp: Account<'info, Mint>,
+    #[account(
+        
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+
+    )]
+    pub vault_x: Account<'info, TokenAccount>, // to hold tokens
+    #[account(
+       
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+
+    )]
+    pub vault_y: Account<'info, TokenAccount>, // to hold tokens
+    #[account(
+        //no need to initialize
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump
+    )]
+    pub config: Account<'info, Config>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit(
+        &mut self,
+        amount: u64, 
+        max_x: u64, 
+        max_y:u64
+    ) -> Result<()> {
+        assert!(amount != 0);
+
+        let (x,y)  = match self.mint_lp.supply == 0 && self.vault_x.amount == 0 {
+            true => (max_x, max_y),
+            false => {
+                let amounts = ConstantProduct::xy_deposit_amounts_from_l(
+                    self.vault_x.amount,
+                    self.vault_y.amount,
+                    self.mint_lp.supply,
+                    amount,
+                    6    
+                ).unwrap();
+                (amounts.x, amounts.y)
+            }
+        };
+        
+        Ok(())
+    }
+}
