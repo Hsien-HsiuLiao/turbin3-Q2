21eefef8d3678c51e936693cf30f36b1c11f0f8e
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-11 20:48:50 -0700

    create initialize instruction

--- anchor-amm/programs/amm/src/lib.rs
+++ anchor-amm/programs/amm/src/lib.rs
@@ -1,16 +1,30 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
+
+mod instructions;
+mod state;
+
+use instructions::*;
 
 declare_id!("7PHpxPwkoM3VaTjvBTz3qKUBKZprGWzQ7mnJKF9c7nNN");
 
 #[program]
 pub mod amm {
+    //use crate::instructions::Initialize;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64, 
+        fee: u16, 
+        authority: Option<Pubkey>,
+
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
