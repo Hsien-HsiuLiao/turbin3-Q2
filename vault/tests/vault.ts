24aceae3cf8a8edcacd770ca5578c9056815766b
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-14 13:56:14 -0700

    add initialize and deposit test

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,16 +1,92 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import { Keypair, PublicKey, SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction, Transaction } from '@solana/web3.js';
+import {  createAccount, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { confirmTransaction } from "@solana-developers/helpers";
 
-describe("vault", () => {
+
+const vaultAddress = new PublicKey("3YJsLgDvMoRHr5ttc19ZVdvTVfFHWE81FVcBgWLBKTFb");
+
+const provider = anchor.AnchorProvider.env();
+const connection = provider.connection;
+
+let vault;
+  let vault_state;
+  let bump;
+
+
+describe("vault", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.vault as Program<Vault>;
 
+  const user = (provider.wallet as anchor.Wallet).payer;
+  const payer = user;
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+   // const tx = await program.methods.initialize().rpc();
+
+   //try catch
+   const initTx = await program.methods
+   .initialize()
+   .accounts({
+       //signer: program.provider.publicKey,
+       signer: payer.publicKey,
+    //   vault,
+   //    vaultState: vaultState.publicKey,
+     //  systemProgram: SystemProgram.programId,
+   })
+   .signers([payer])
+   .rpc();
+
+    console.log("Your transaction signature", initTx);
+
+      const balance = await connection.getBalance(payer.publicKey);
+      console.log(`Payer balance: ${balance}`);
+
+      [vault_state, bump] = PublicKey.findProgramAddressSync([
+        Buffer.from("state"),
+        payer.publicKey.toBuffer(),
+    ], program.programId);
+    
+    [vault, bump] = PublicKey.findProgramAddressSync([
+      Buffer.from("vault"),
+      vault_state.toBuffer(),
+    ], program.programId);
+
+      /*  */
+        let sol = 2;
+        let vault_airdrop_tx_sig = await connection.requestAirdrop(
+          vault,
+          sol * LAMPORTS_PER_SOL
+        );
+
+        console.log("✍🏾 Airdrop Signature: ", vault_airdrop_tx_sig);
+
+ let confirmedAirdrop = await confirmTransaction(connection, vault_airdrop_tx_sig, "confirmed");
+ 
+ 
+
+ /*  console.log(`🪂 Airdropped ${sol} SOL to ${vault.toBase58()}`);
+  console.log("✅ Tx Signature: ", confirmedAirdrop); */
+
+        const accounts = {
+          signer: payer.publicKey,
+          vaultState: vault_state,
+          vault: vault,
+          systemProgram: SystemProgram.programId,      
+         }
+
+      const depositTx = await program.methods
+      .deposit(new anchor.BN(7))
+      .accounts({
+        ...accounts            })
+      .signers([payer])
+      .rpc();
+
+
   });
 });
