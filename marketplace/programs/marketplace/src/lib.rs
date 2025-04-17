1dc5773226075d024224f54adcf79a9c7fe11cbe
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-17 10:55:57 -0700

    add ix

--- marketplace/programs/marketplace/src/lib.rs
+++ marketplace/programs/marketplace/src/lib.rs
@@ -16,10 +16,21 @@ declare_id!("FXUQwDsKJNrYFsfiUokPbH4BSrZtoC9m8HpoiMvYxtSE");
 pub mod marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee:u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
         Ok(())
     }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+        Ok(())
+    }
+
+    /* pub fn purchase(ctx: Context<Purchase>, ) -> Result<()> {
+        
+        Ok(())
+    } */
 }
 
-//get markeetplace pda, pda will save configuration for marketplace (admin, fee, ...)
+//get marketplace pda, pda will save configuration for marketplace (admin, fee, ...)
