9c1f73e1e2d9cf73a68f893340c4a7fe2429bc8f
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-25 21:58:18 -0700

    add stake ix

---
+++ staking/programs/staking/src/instructions/stake.rs
@@ -0,0 +1,86 @@
+//1:04:00 video
+use anchor_lang::prelude::*;
+
+use anchor_spl::token::{Mint, Token, TokenAccount};
+//add imports 1:17;00
+
+use crate::state::StakeAccount;
+
+
+#[derive(Accounts)]
+pub struct Stake<'info> {
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub mint: Account<'info, Mint>,
+    pub collection_mint: Account<'info, Mint>,
+
+    #[account(
+        mut, 
+        associated_token::mint = mint, 
+        associated_token::authority = user
+    )]
+    pub mint_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        seeds = [
+            //standard seeds
+            b"metadata",
+            metadata_program.key().as_ref(),
+            mint.key().as_ref()  //unique
+
+        ],
+        seeds::program = metadata_program.key(),
+        bump, 
+        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().verified,
+    )]
+    pub metadata: Account<'info, MetadataAccount>,
+    #[account(
+        seeds = [
+            //standard seeds
+            b"metadata",
+            metadata_program.key().as_ref(),
+            mint.key().as_ref(),
+            b"edition",  
+
+        ],
+        seeds::program = metadata_program.key(),
+        bump, 
+    )]
+    pub master_edition: Account<'info, MasterEditionAccount>,
+
+    #[account(
+        mut, //to account for nfts
+        seeds = [b"user", user.key().as_ref()], 
+        bump = user_account.bump, 
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(
+        seeds = [b"config"],
+        bump = config.bump, //why config.bump, because not init?
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        init, 
+        payer = user, 
+        seeds = [b"stake", config.key().as_ref(), mint.key().as_ref()],
+        bump, 
+        space = 8 + StakeAccount::INIT_SPACE
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+    pub metadata_program: Program<'info, Metadata>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>
+}
+
+impl <'info> Stake<'info> {
+    pub fn stake(&mut self, ) -> Result<()>{
+
+        
+        Ok(())
+    }
+}
