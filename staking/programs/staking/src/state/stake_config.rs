471eb80c02f8be5e58070b165f911a6eedf29343
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-04-25 20:59:37 -0700

    add comment for points_per_stake

--- staking/programs/staking/src/state/stake_config.rs
+++ staking/programs/staking/src/state/stake_config.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct StakeConfig {
-    pub points_per_stake: u8,
+    pub points_per_stake: u8, //rewards points for users
     pub max_stake: u8,
     pub freeze_period: u32, //if someone wants to unstake, need to check this first
     pub rewards_bump: u8,
