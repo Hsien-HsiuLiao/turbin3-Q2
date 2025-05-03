2b0ce96392cf774e8d25ffc9da33410a7cc03a43
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-03 09:44:20 -0700

    replace price with rental_rate

--- sol-parking-marketplace/programs/marketplace/src/state/listing.rs
+++ sol-parking-marketplace/programs/marketplace/src/state/listing.rs
@@ -4,11 +4,10 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Listing {
     pub maker: Pubkey,
-    pub price: u64, 
     pub bump: u8, 
     #[max_len(32)] 
     pub address: String,
-    pub rental_rate: u64,
+    pub rental_rate: u64, //per hour
     pub sensor_id: u64, 
     pub reserved_by: Option<Pubkey>, 
     pub reservation_duration: Option<u64>,
@@ -27,5 +26,7 @@ pub enum ParkingSpaceStatus {
 impl Space for ParkingSpaceStatus {
     //const LEN: usize = 1;
                                     // rust-analyer popup mentioned 42, trying 42
-    const INIT_SPACE: usize = 42; // Since we are using u8 representation
+    // const INIT_SPACE: usize = 42; // Since we are using u8 representation
+    const INIT_SPACE: usize = 1; 
+
 }
