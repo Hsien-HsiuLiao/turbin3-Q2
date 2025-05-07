1b82b60c9076df86ea063e36e3295b789c9694a9
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-06 20:40:28 -0700

    update ix

--- sol-parking-marketplace/programs/marketplace/src/lib.rs
+++ sol-parking-marketplace/programs/marketplace/src/lib.rs
@@ -17,21 +17,26 @@ declare_id!("FXUQwDsKJNrYFsfiUokPbH4BSrZtoC9m8HpoiMvYxtSE");
 pub mod marketplace {
     use super::*;
 
-     pub fn initialize(ctx: Context<Initialize>, name: String, fee:u16) -> Result<()> {
+     pub fn initialize(ctx: Context<Initialize>, name: String, fee:u32) -> Result<()> {
         ctx.accounts.init(name, fee, &ctx.bumps)?;
         Ok(())
     }
 
-    pub fn list(ctx: Context<List>, address: String, rental_rate: u16, sensor_id: String, latitude:f64, longitude:f64) -> Result<()> {
-        ctx.accounts.create_listing(address, rental_rate, sensor_id, latitude, longitude, &ctx.bumps)?;
+    pub fn list(ctx: Context<List>, address: String, rental_rate: u32, sensor_id: String, latitude:f64, longitude:f64, additional_info: Option<String>) -> Result<()> {
+        ctx.accounts.create_listing(address, rental_rate, sensor_id, latitude, longitude, additional_info, &ctx.bumps)?;
         Ok(())
     }
 
+    //pub fn update_listing()
+
+    //pub fn set_notification_settings() -> 
     pub fn reserve(ctx: Context<Reserve>, duration: u16) -> Result<()> {
         //pass in sensor_id if needed
         ctx.accounts.reserve_listing(duration)?;
         Ok(())
     }
+
+    //pub fn update_reservation()
 
     pub fn sensor_change<'a>(ctx: Context<SwitchboardFeed>) -> Result<()> { //when driver arrives or leaves
         // Feed account data
@@ -45,12 +50,15 @@ pub mod marketplace {
         // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html
         let feed = PullFeedAccountData::parse(feed_account).unwrap();
         // Log the value
-        msg!("sensor data, distance_in_cm: {:?}", feed.value());
+        msg!("sensor data, distance_in_cm: {:?}", feed.value().unwrap());
         Ok(())
 
 }
 
-    //pub fn confirm_parking() {//driver scans QR code to confirm arrival and parking}
+    //pub fn confirm_parking() {//driver scans QR code to confirm arrival and parking}, 
+    //should also send alert to homeowner
+    //user story 1c
+    //driver receives confirmation
 
 
 }
