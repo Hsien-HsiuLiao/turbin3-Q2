1be1cc53714fc5761f4e2a774f81cd8d3aafce74
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-09 17:54:07 -0700

    update accounts

--- sol-parking-marketplace/programs/marketplace/src/instructions/switchboard_feed.rs
+++ sol-parking-marketplace/programs/marketplace/src/instructions/switchboard_feed.rs
@@ -11,6 +11,29 @@ pub struct SwitchboardFeed<'info> {
     //safety check: Struct field "feed" is unsafe, but is not documented.add ///CHECK
         /// CHECK: via switchboard sdk
     pub feed: AccountInfo<'info>,
+
+    #[account(
+        //derivation
+        seeds = [b"marketplace", marketplace.name.as_bytes()], //make generic
+        bump = marketplace.bump
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [marketplace.key().as_ref(), 
+        maker.key().as_ref()
+        //&sensor_id.to_le_bytes()
+        ], 
+        bump, 
+        realloc = 8 + Listing::INIT_SPACE,
+        realloc::payer = maker, 
+        realloc::zero = true, 
+    )]
+    pub listing: Account<'info, Listing>,
+    pub system_program: Program<'info, System>,
+
 }
 
 impl <'info> SwitchboardFeed<'info> {
