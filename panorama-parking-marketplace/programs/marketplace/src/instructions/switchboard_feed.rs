f88f89fccd1a0ac0354540178520691cfa74f017
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-14 23:21:13 -0700

    refactor sensor_change

--- panorama-parking-marketplace/programs/marketplace/src/instructions/switchboard_feed.rs
+++ panorama-parking-marketplace/programs/marketplace/src/instructions/switchboard_feed.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
+use switchboard_on_demand::prelude::rust_decimal::Decimal;
 
 
 
@@ -38,8 +39,53 @@ pub struct SwitchboardFeed<'info> {
 
 impl <'info> SwitchboardFeed<'info> {
    
-//    pub fn sensor_change(ctx: Context<SwitchboardFeed>) -> Result<()> { //when driver arrives or leaves
-   
+    pub fn sensor_change(&mut self) -> Result<()> { //when driver arrives or leaves
+      // Feed account data
+      let feed_account = self.feed.data.borrow();//ctx.accounts.feed.data.borrow();
+      // msg!("feed_account.: {:?}", feed_account);
+
+       // Verify that this account is the intended one by comparing public keys
+       // if ctx.accounts.feed.key != &specific_pubkey {
+       //     throwSomeError
+       // }
+       //
+
+       // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html
+       let feed = PullFeedAccountData::parse(feed_account).unwrap();
+       // Log the value
+      // msg!("feeed.: {:?}", feed);
+       msg!("sensor data, distance_in_cm: {:?}", feed.value().unwrap());
+       //distance < 30 cm, car parked, >30cm car left space
+
+       //when driver leaves, sensor detects change. a server function monitoring for changes (or anchor test) will
+       // call this instruction, get the data and verify value is over x cm, then msg homeowner and parkingspace status updated
+
+       //user story 1c
+   //driver receives confirmation
+
+   let distance= feed.value().unwrap();
+
+   // Check if the distance indicates the car has left the space
+   if distance > Decimal::from(30) {
+       let listing = &mut self.listing;//&mut ctx.accounts.listing;
+
+       // Update the parking space status to Available
+       listing.parking_space_status = ParkingSpaceStatus::Available;
+
+       // Notify the homeowner of the change
+
+     //  msg!("Parking space is now available for listing: {:?}", listing);
+
+       //check if driver left on time and/or within 5 min grace period
+       //if not charge a penalty and transfer to homeowner
+
+   }
+  /*  if distance <= Decimal::from(30){
+       //cannot yet confirm parking space in use, driver needs to also scan QR code to confirm
+   } */
+
+        Ok(())
+    }
 
     
 }
--- panorama-parking-marketplace/programs/marketplace/src/lib.rs
+++ panorama-parking-marketplace/programs/marketplace/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(unexpected_cfgs)]
 
 use anchor_lang::prelude::*;
-use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
+//use switchboard_on_demand::on_demand::accounts::pull_feed::PullFeedAccountData;
 
 mod instructions;
 mod state;
@@ -18,7 +18,7 @@ declare_id!("FXUQwDsKJNrYFsfiUokPbH4BSrZtoC9m8HpoiMvYxtSE");
 #[program]
 pub mod marketplace {
 
-    use switchboard_on_demand::prelude::rust_decimal::Decimal;
+  //  use switchboard_on_demand::prelude::rust_decimal::Decimal;
 
     use crate::state::ParkingSpaceStatus;
 
@@ -108,50 +108,7 @@ pub mod marketplace {
 
     pub fn sensor_change<'a>(ctx: Context<SwitchboardFeed>) -> Result<()> {
         //when driver arrives or leaves
-        // Feed account data
-        let feed_account = ctx.accounts.feed.data.borrow();
-       // msg!("feed_account.: {:?}", feed_account);
-
-        // Verify that this account is the intended one by comparing public keys
-        // if ctx.accounts.feed.key != &specific_pubkey {
-        //     throwSomeError
-        // }
-        //
-
-        // Docs at: https://switchboard-on-demand-rust-docs.web.app/on_demand/accounts/pull_feed/struct.PullFeedAccountData.html
-        let feed = PullFeedAccountData::parse(feed_account).unwrap();
-        // Log the value
-       // msg!("feeed.: {:?}", feed);
-        msg!("sensor data, distance_in_cm: {:?}", feed.value().unwrap());
-        //distance < 30 cm, car parked, >30cm car left space
-
-        //when driver leaves, sensor detects change. a server function monitoring for changes (or anchor test) will
-        // call this instruction, get the data and verify value is over x cm, then msg homeowner and parkingspace status updated
-
-        //user story 1c
-    //driver receives confirmation
-
-    let distance= feed.value().unwrap();
-
-    // Check if the distance indicates the car has left the space
-    if distance > Decimal::from(30) {
-        let listing = &mut ctx.accounts.listing;
-
-        // Update the parking space status to Available
-        listing.parking_space_status = ParkingSpaceStatus::Available;
-
-        // Notify the homeowner of the change
-
-      //  msg!("Parking space is now available for listing: {:?}", listing);
-
-        //check if driver left on time and/or within 5 min grace period
-        //if not charge a penalty and transfer to homeowner
-
-    }
-   /*  if distance <= Decimal::from(30){
-        //cannot yet confirm parking space in use, driver needs to also scan QR code to confirm
-    } */
-
+        ctx.accounts.sensor_change()?;
         Ok(())
     }
 
