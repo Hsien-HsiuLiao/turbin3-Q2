4a4151d8fa6e3370f58bc981d06e46865496859c
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-24 16:02:57 -0700

    remove unneeded code

--- capstone/vercel-depin-parking/src/components/homeowners/homeowner-list-feature.tsx
+++ capstone/vercel-depin-parking/src/components/homeowners/homeowner-list-feature.tsx
@@ -1,16 +1,10 @@
 'use client'
 
-import { WalletButton } from '../solana/solana-provider'
 
-import { redirect } from 'next/navigation'
-import { useWalletUi } from '@wallet-ui/react'
 
 export default function HomeownerListFeature() {
-  const { account } = useWalletUi()
 
-  if (account) {
-    return redirect(`/account/${account.address.toString()}`)
-  }
+  
 
   return (
     <div className="hero py-[64px]">
