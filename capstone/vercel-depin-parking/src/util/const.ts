32f914fc16f1e8c798c034ad21d342a8e90f076f
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-25 16:07:33 -0700

    work on provider

--- capstone/vercel-depin-parking/src/util/const.ts
+++ capstone/vercel-depin-parking/src/util/const.ts
@@ -1,6 +1,10 @@
 import { Connection, PublicKey } from "@solana/web3.js";
 import { Program } from "@coral-xyz/anchor";
 import { IDL, Marketplace } from "./depin-panorama-parking-marketplace";
+import { AnchorProvider } from '@coral-xyz/anchor';
+import { AnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+
+
 
 export const CONNECTION = new Connection(process.env.NEXT_PUBLIC_RPC ? process.env.NEXT_PUBLIC_RPC : 'https://api.devnet.solana.com',  {
     wsEndpoint: process.env.NEXT_PUBLIC_WSS_RPC ? process.env.NEXT_PUBLIC_WSS_RPC : "wss://api.devnet.solana.com",
@@ -9,5 +13,10 @@ export const CONNECTION = new Connection(process.env.NEXT_PUBLIC_RPC ? process.e
 
 export const PROGRAM_ID = new PublicKey('FXUQwDsKJNrYFsfiUokPbH4BSrZtoC9m8HpoiMvYxtSE');
 
-export const PROGRAM = new Program<Marketplace>(IDL, /* PROGRAM_ID, */ { connection: CONNECTION })
+    const { connection } = useConnection();
+    const wallet = useWallet()
+
+const provider = new AnchorProvider(connection, wallet as AnchorWallet, { commitment: 'confirmed' })
+
+export const PROGRAM = new Program<Marketplace>(IDL, provider/* PROGRAM_ID,  { connection: CONNECTION }*/)
 
