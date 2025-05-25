36231827cc177be62807dc881fd0202dce7a79aa
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-25 11:44:24 -0700

    update driver page

---
+++ capstone/vercel-depin-parking/src/components/drivers/driver-feature.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+
+
+export default function DriverFeature() {
+
+
+
+  return (
+    <div className="hero py-[64px]">
+      <div className="hero-content text-center">
+        Here's a list of parking spaces to reserve
+      </div>
+    </div>
+  )
+}
