e96a8c2d8802d207b018e358c0da518ad1047d88
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-27 10:01:59 -0700

    react/no-unescaped-entities off

--- capstone/create-solana-dapp-legacy-next-tailwind/eslint.config.mjs
+++ capstone/create-solana-dapp-legacy-next-tailwind/eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript
       '@typescript-eslint/no-unused-vars' : 'off',
       '@typescript-eslint/no-unused-expressions' : 'off',
       'prefer-const' : 'off',
-      'react-hooks/rules-of-hooks' : 'off'
+      'react-hooks/rules-of-hooks' : 'off',
+      'react/no-unescaped-entities' : 'off',
     },
   },
 ]
