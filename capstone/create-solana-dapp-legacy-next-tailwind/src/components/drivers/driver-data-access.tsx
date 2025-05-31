74e284e6e470b27481d8c9779a75d7b0cf052f39
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-05-31 12:30:18 -0700

    remove unneeded code

--- capstone/create-solana-dapp-legacy-next-tailwind/src/components/drivers/driver-data-access.tsx
+++ capstone/create-solana-dapp-legacy-next-tailwind/src/components/drivers/driver-data-access.tsx
@@ -3,10 +3,8 @@
 import * as anchor from '@coral-xyz/anchor';
 
 import { getMarketplaceProgram, getMarketplaceProgramId } from "../../util/marketplace-exports";
-//import { PROGRAM, PROGRAM_ID } from "../../util/const";
 import { AnchorProvider } from '@coral-xyz/anchor';
 import { AnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
-//import { useConnect } from '@wallet-ui/react'; //instead of useConnection?
 
 import { Cluster, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { useMutation, useQuery } from "@tanstack/react-query";
@@ -14,27 +12,10 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from 'sonner'
 
 import { useCluster } from "../cluster/cluster-data-access";
-//import { UiWalletAccount, useWalletUiCluster } from '@wallet-ui/react';
 import { /* useAnchorProvider */SolanaProvider } from "../solana/solana-provider";
-//import { useTransactionToast } from "../ui/ui-layout";
 import { useTransactionToast } from '../use-transaction-toast'
 
 import { useMemo } from "react";
-
-interface CreateListingArgs {
-    address: string;              // Address (String)
-    rentalRate: number;           // Rental rate (u32)
-    sensorId: string;             // Sensor ID (String)
-    latitude: number;             // Latitude (f64)
-    longitude: number;            // Longitude (f64)
-    additionalInfo?: string;      // Additional information (Option<String>)
-    availabilityStart: anchor.BN;    // Availability start (i64)
-    availabilityEnd: anchor.BN;      // Availability end (i64)
-    email: string;                // Email (String)
-    phone: string;
-    homeowner1: PublicKey;
-
-}
 
 
 
@@ -42,7 +23,6 @@ export function useMarketplaceProgram() {
     const { connection } = useConnection();
 
     const { cluster } = useCluster();
-   // const { cluster } = useWalletUiCluster()
 
     const transactionToast = useTransactionToast();
     //const provider = useAnchorProvider();
@@ -63,109 +43,44 @@ export function useMarketplaceProgram() {
         queryFn: () => program.account.listing.all(),
     });
 
-    console.log("accounts", accounts);
+   // console.log("accounts", accounts);
 
     const getProgramAccount = useQuery({
         queryKey: ["get-program-account", { cluster }],
         queryFn: () => connection.getParsedAccountInfo(programId),
     });
 
-    const marketplace_name = "DePIN PANORAMA PARKING";
-
-    let marketplace: PublicKey;
-    let marketplaceBump;
+   
     
-    [marketplace, marketplaceBump] = PublicKey.findProgramAddressSync(
-      [Buffer.from("marketplace"), Buffer.from(marketplace_name)],
-      program.programId
-    );
-
-    const createListing = useMutation<string, Error, CreateListingArgs>({
-        mutationKey: ["journalEntry", "create", { cluster }],
-        mutationFn: async ({address,
-            rentalRate,
-            sensorId,
-            latitude,
-            longitude,
-            additionalInfo,
-            availabilityStart,
-            availabilityEnd,
-            email,
-            phone,
-        homeowner1}) => {
-          console.log({
-            address,
-            rentalRate,
-            sensorId,
-            latitude,
-            longitude,
-            additionalInfo,
-            availabilityStart,
-            availabilityEnd,
-            email,
-            phone,
-            homeowner1
-        });
-
-        console.log("Marketplace PublicKey:", marketplace);
-console.log("Homeowner PublicKey:", homeowner1);
-                const listingPDA = PublicKey.findProgramAddressSync(
-                    [marketplace.toBuffer(), homeowner1.toBuffer()],
-                    program.programId
-                  )[0];
-            return  program.methods.list(
-                address,            // string
-                rentalRate,         // number
-                sensorId,           // string
-                latitude,           // number
-                longitude,          // number
-                additionalInfo ?? null,     // string (optional)
-                availabilityStart,   // number (i64)
-                availabilityEnd,     // number (i64)
-                email,              // string
-                phone,                 // string
-                
-            )
-            .accountsPartial({
-               // maker: homeowner1,
-                marketplace: marketplace,
-                //     listing: listing
-              })
-            //  .signers([homeowner1])
-            .rpc();
-    },
-        onSuccess: (signature) => {
-            transactionToast(signature);
-            accounts.refetch();
-        },
-        onError: (error) => {
-            toast.error(`Failed to create journal entry: ${error.message}`);
-        },
-  });
+    
 
 return {
     program,
     programId,
     accounts,
     getProgramAccount,
-    createListing
+    
 };
 }
 
-//export function useJournalProgramAccount({ account }: { account: PublicKey }) {
   export function useMarketplaceProgramAccount({ account }: { account: PublicKey }) {
 
   const { cluster } = useCluster();
-//  const { cluster } = useWalletUiCluster()
 
   const transactionToast = useTransactionToast();
- // const { program, accounts } = useJournalProgram();
   const { program, accounts } = useMarketplaceProgram();
 
 
   const accountQuery = useQuery({
     queryKey: ["journal", "fetch", { cluster, account }],
     queryFn: () => program.account.listing.fetch(account),
+  });
+
+  //instead of fetching account, need to fetch all marketpalce listings
+  //    const listings = await program.account.listing.all();
+  const listingsQuery = useQuery({
+    queryKey: ["listings", "fetch", { cluster, account }],
+    queryFn: () => program.account.listing.all(),
   });
 
   const marketplace_name = "DePIN PANORAMA PARKING";
@@ -181,90 +96,9 @@ let marketplaceBump;
 
 
 
-
- // const updateEntry = useMutation<string, Error, CreateEntryArgs>({
-    const updateListing = useMutation<string, Error, CreateListingArgs>({
-
-    mutationKey: ["journalEntry", "update", { cluster }],
-    mutationFn: async ({
-      address,
-      rentalRate,
-      sensorId,
-      latitude,
-      longitude,
-      additionalInfo,
-      availabilityStart,
-      availabilityEnd,
-      email,
-      phone,
-  homeowner1}) => {
-    const listingPDA = PublicKey.findProgramAddressSync(
-      [marketplace.toBuffer(), homeowner1.toBuffer()],
-      program.programId
-    )[0];
-
-    const listing = PublicKey.findProgramAddressSync(
-      [marketplace.toBuffer(), homeowner1.toBuffer()],
-      program.programId
-    )[0];
-
-      return program.methods.updateListing( address,            // string
-        rentalRate * LAMPORTS_PER_SOL,         // number
-        sensorId,           // string
-        latitude,           // number
-        longitude,          // number
-        additionalInfo ?? null,     // string (optional)
-        availabilityStart,   // number (i64)
-        availabilityEnd,     // number (i64)
-        email,              // string
-        phone,                 // string
-        )
-        .accountsPartial({
-           maker: homeowner1,
-           marketplace: marketplace,
-                listing: listing
-         })
-         .rpc();
-    },
-    onSuccess: (signature) => {
-      transactionToast(signature);
-      accounts.refetch();
-    },
-    onError: (error) => {
-      toast.error(`Failed to update listing: ${error.message}`);
-    },
-  });
-// Define the input type for the mutation function
-interface DeleteListingInput {
-  homeowner1: PublicKey; // Adjust the type based on your actual data structure
-}
-
-  const deleteListing = useMutation({
-    mutationKey: ["journal", "deleteEntry", { cluster, account }],
-    mutationFn: async ({homeowner1}:DeleteListingInput) => {
-      const listing = PublicKey.findProgramAddressSync(
-        [marketplace.toBuffer(), homeowner1.toBuffer()],
-        program.programId
-      )[0];
-
-      return program.methods.deleteListing()
-    .accountsPartial({
-      maker: homeowner1, //
-      marketplace: marketplace,
-      listing: listing,
- //     owner: homeowner1 //
-    })
-    .rpc()
-  },
-    onSuccess: (tx) => {
-      transactionToast(tx);
-      return accounts.refetch();
-    },
-  });
-
   return {
     accountQuery,
-    updateListing,
-    deleteListing,
+    listingsQuery
+    
   };
 }
